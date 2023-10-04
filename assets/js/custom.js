134c08dd8c81aacfdb45ae360d0005c94cefeed1
Rubel Mahmud <vxlrubel@gmail.com>
2023-10-04 21:29:01 +0600

    update

---
+++ assets/js/custom.js
@@ -0,0 +1,16 @@
+(function($){
+
+    class SDW {
+        init(){
+            this.header();
+        }
+
+        header(){
+            
+        }
+    }
+    $(document).ready(function () {
+        const sdw = new SDW();
+        sdw.init();
+    });
+})(jQuery);
