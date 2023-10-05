f1677e2a5d2a8c4b5711999791b84136eaac70c6
Rubel Mahmud <vxlrubel@gmail.com>
2023-10-05 23:11:37 +0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,7 +6,25 @@
         }
 
         header(){
-            
+            // clone menu
+            $('.menu').clone().attr('class', 'mobile-menu').appendTo('.responsive-menu');
+
+            // add toggle class if has submenu
+            $('.responsive-menu').find('li.has-submenu').children('a').addClass('has-submenu-link').append('<span class="submenu-toggle">submen toggle</span>');
+
+            // slide toggle submenu when click '.has-submenu-link'
+            $('.has-submenu-link').on('click','.submenu-toggle', function(e){
+                e.preventDefault();
+                $(this).stop().toggleClass('show');
+                $(this).parent('a').siblings('ul.submenu').stop().slideToggle(300);
+            });
+
+            // open responsive menu
+            $('#menu-toggle').on('click', function(e){
+                e.preventDefault();
+                $('.responsive-menu').stop().toggleClass('show');
+            });
+        
         }
     }
     $(document).ready(function () {
